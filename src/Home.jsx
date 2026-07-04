2cc84f3c93428f770d19ff931545d10bcf8fe252
cbgalileo <cbrice@plu.edu>
2026-07-04 15:55:55 -0700

    finished general layout of home screen
    - added practice setup panel elements and root elements for library
    - breadcrumb trail for library is working as well

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,55 @@
 import { useState } from 'react'
 import './App.css'
+import Library from './Library';
 
 function Home()
 {
     return (
         <>
             <div className='home'>
-                <div className='library'>
-                    <div className='library-header'>
-                        <p>Library </p>
-                        <p className='breadcrumb-trail'></p>
-                    </div>
+                <Library></Library>
 
-                    <div className='library-content'>
-                        
-                    </div>
-                </div>
-
-                <div className='practice-setup'>
+                <div className='setup-panel'>
                     <div className='setup-header'>
                         <p>Session Setup</p>
+                    </div>
+
+                    <div className='setup-content'>
+                        <div className='setup-item'>
+                            <label>Mode</label>
+                            
+                            <div>
+                                <label>
+                                    <input type="radio" name="mode" value="manual" defaultChecked />
+                                    <span>Manual</span>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label>
+                                    <input type="radio" name="mode" value="timed" />
+                                    <span>Timed</span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className='setup-item'>
+                            <label>Timer</label>
+                            <div className='timer'>
+                                <input type="number" min={1} defaultValue={30} />
+                                <span>seconds</span>
+                            </div>
+                        </div>
+
+                        <div className='setup-item'>
+                            <label>Selected Folders</label>
+                            <div className='selected-folders'>
+
+                            </div>
+                        </div>
+
+
+                        <button>Start</button>
                     </div>
                 </div>
             </div>
